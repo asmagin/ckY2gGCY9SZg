2f92d41c917a849759e7dd3157502a7b429386d2
Zhora Grigoryan <zhora.grigoryan@bausch.com>
2025-04-21 17:12:59 +0400

    test: testing how to change the default styles

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,37 +1,41 @@
-document.addEventListener('DOMContentLoaded', function () {    // const forgotPassword = document.getElementById('forgotPassword');
-    const buttons = document.querySelector('.buttons');
+alert('Script working')
+const observer = new MutationObserver(() => {
     const forgotPassword = document.getElementById('forgotPassword');
+    const createAccount = document.getElementById('createAccount');
 
-    if (forgotPassword && buttons && buttons.parentNode) {
-        buttons.parentNode.insertBefore(forgotPassword, buttons.nextSibling);
-        // observer.disconnect();
+    console.log({
+        forgotPassword,
+        createAccount
+    })
+
+    forgotPassword.classList.add('link')
+    forgotPassword.innerHTML = 'Forgot Password?';
+
+    createAccount.classList.add('link');
+    forgotPassword.innerHTML = 'Sign Up';
+
+
+    const wrapper = document.createElement('div');
+    wrapper.className = 'input-wrapper';
+    wrapper.style.textAlign = 'center';
+    wrapper.style.marginTop = '16px';
+
+    const separator = document.createElement('div');
+    separator.className = 'separator';
+    separator.innerHTML = `<hr />Don’t have an account?<hr />`;
+
+    wrapper.appendChild(forgotPassword);
+    wrapper.appendChild(separator);
+    wrapper.appendChild(createAccount);
+
+    const nextButton = document.getElementById('next');
+    console.log({
+        nextButton,
+        nextButtonParent: nextButton.parentNode
+    })
+    if (nextButton && nextButton.parentNode) {
+        nextButton.parentNode.insertBefore(wrapper, nextButton.nextSibling);
     }
-    //
-    // const forgotPassword = document.getElementById('forgotPassword');
-    // const createAccount = document.getElementById('createAccount');
-    //
-    // forgotPassword.classList.add('link')
-    // forgotPassword.innerHTML = 'Forgot Password?';
-    //
-    // createAccount.classList.add('link');
-    // forgotPassword.innerHTML = 'Sign Up';
-    //
-    //
-    // const wrapper = document.createElement('div');
-    // wrapper.className = 'input-wrapper';
-    // wrapper.style.textAlign = 'center';
-    // wrapper.style.marginTop = '16px';
-    //
-    // const separator = document.createElement('div');
-    // separator.className = 'separator';
-    // separator.innerHTML = `<hr />Don’t have an account?<hr />`;
-    //
-    // wrapper.appendChild(forgotPassword);
-    // wrapper.appendChild(separator);
-    // wrapper.appendChild(createAccount);
-
-    // const buttons = document.querySelector('.buttons');
-    // if (buttons && buttons.parentNode) {
-    //     buttons.parentNode.insertBefore(wrapper, buttons.nextSibling);
-    // }
 });
+
+observer.observe(document.body, { childList: true, subtree: true });
