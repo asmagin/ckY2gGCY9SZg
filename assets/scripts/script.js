d61db30e9feef712cbdc4020a27ca1b33903c837
Zhora Grigoryan <zhora.grigoryan@bausch.com>
2025-04-21 17:17:17 +0400

    test: testing how to change the default styles

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,56 +1,44 @@
+
 alert('Script working')
 const observer = new MutationObserver(() => {
-    console.log({ working: 1})
     const forgotPassword = document.getElementById('forgotPassword');
-    const buttons = document.querySelector('.buttons');
+    const createAccount = document.getElementById('createAccount');
 
-    if (forgotPassword && buttons && buttons.parentNode) {
-        buttons.parentNode.insertBefore(forgotPassword, buttons.nextSibling);
+    console.log({
+        forgotPassword,
+        createAccount
+    })
+
+    forgotPassword.classList.add('link')
+
+    forgotPassword.innerHTML = 'Forgot Password?';
+
+    createAccount.classList.add('link');
+    forgotPassword.innerHTML = 'Sign Up';
+
+
+    const wrapper = document.createElement('div');
+    wrapper.className = 'input-wrapper';
+    wrapper.style.textAlign = 'center';
+    wrapper.style.marginTop = '16px';
+
+    const separator = document.createElement('div');
+    separator.className = 'separator';
+    separator.innerHTML = `<hr />Don’t have an account?<hr />`;
+
+    wrapper.appendChild(forgotPassword);
+    wrapper.appendChild(separator);
+    wrapper.appendChild(createAccount);
+
+    const nextButton = document.getElementById('next');
+    console.log({
+        nextButton,
+        nextButtonParent: nextButton.parentNode
+    })
+    if (nextButton && nextButton.parentNode) {
+        nextButton.parentNode.insertBefore(wrapper, nextButton.nextSibling);
         observer.disconnect();
     }
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
-
-// alert('Script working')
-// const observer = new MutationObserver(() => {
-//     const forgotPassword = document.getElementById('forgotPassword');
-//     const createAccount = document.getElementById('createAccount');
-//
-//     console.log({
-//         forgotPassword,
-//         createAccount
-//     })
-//
-//     forgotPassword.classList.add('link')
-
-//     forgotPassword.innerHTML = 'Forgot Password?';
-//
-//     createAccount.classList.add('link');
-//     forgotPassword.innerHTML = 'Sign Up';
-//
-//
-//     const wrapper = document.createElement('div');
-//     wrapper.className = 'input-wrapper';
-//     wrapper.style.textAlign = 'center';
-//     wrapper.style.marginTop = '16px';
-//
-//     const separator = document.createElement('div');
-//     separator.className = 'separator';
-//     separator.innerHTML = `<hr />Don’t have an account?<hr />`;
-//
-//     wrapper.appendChild(forgotPassword);
-//     wrapper.appendChild(separator);
-//     wrapper.appendChild(createAccount);
-//
-//     const nextButton = document.getElementById('next');
-//     console.log({
-//         nextButton,
-//         nextButtonParent: nextButton.parentNode
-//     })
-//     if (nextButton && nextButton.parentNode) {
-//         nextButton.parentNode.insertBefore(wrapper, nextButton.nextSibling);
-//     }
-// });
-//
-// observer.observe(document.body, { childList: true, subtree: true });
