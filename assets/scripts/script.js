d6f1e5740be6896cf386e582c6a66583eee8be34
Zhora Grigoryan <zhora.grigoryan@bausch.com>
2025-04-21 17:39:22 +0400

    test: testing how to change the default styles

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,12 @@
 let attempts = 0;
 const maxAttempts = 30;
 
-const interval = setInterval(() => {
+function tryMoveAuthLinks() {
     const forgotPassword = document.getElementById('forgotPassword');
     const createAccount = document.getElementById('createAccount');
     const nextButton = document.getElementById('next');
-    if (forgotPassword && createAccount && nextButton) {
-        clearInterval(interval);
 
+    if (forgotPassword && createAccount && nextButton) {
         forgotPassword.classList.add('link');
         forgotPassword.innerHTML = 'Forgot Password?';
 
@@ -28,11 +27,15 @@ const interval = setInterval(() => {
         wrapper.appendChild(createAccount);
 
         nextButton.parentNode.insertBefore(wrapper, nextButton.nextSibling);
+        return; // ✅ Done, stop recursion
     }
 
-    if (++attempts > maxAttempts) {
-        clearInterval(interval);
+    if (++attempts <= maxAttempts) {
+        setTimeout(tryMoveAuthLinks, 1);
+    } else {
         console.warn('Auth elements not found in time');
     }
-}, 10);
+}
+
+tryMoveAuthLinks()
 
