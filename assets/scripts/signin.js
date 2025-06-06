efc887c977fe55f12029aa3220e98ba689a0e04b
Zhora Grigoryan <zhora.grigoryan@bausch.com>
2025-06-06 14:50:53 +0400

    test: the forgot page

--- assets/scripts/signin.js
+++ assets/scripts/signin.js
@@ -1,6 +1,6 @@
 const config = {
     maxAttempts: 30,
-    checkInterval: 100, // ms
+    checkInterval: 50, // ms
     defaultHeading: "WELCOME BACK!",
     separatorText: "Don't have an account?"
 };
