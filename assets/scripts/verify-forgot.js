53e9caf44daa7a120b417a47a80e89c44077e1ea
Zhora Grigoryan <zhora.grigoryan@bausch.com>
2025-06-09 13:54:01 +0400

    feat: added loader

--- assets/scripts/verify-forgot.js
+++ assets/scripts/verify-forgot.js
@@ -84,7 +84,6 @@ $(document).ready(function () {
     });
 
     waitForButtonEnabled('continue').then(button => {
-        $('.verifying_blurb').addClass('working')
         button.click();
     });
 });
