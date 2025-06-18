8218afd547d59557e5ee49ae718010ca27310d95
Zhora Grigoryan <zhora.grigoryan@bausch.com>
2025-06-18 12:41:06 +0400

    test

--- assets/scripts/verify-signin.js
+++ assets/scripts/verify-signin.js
@@ -79,7 +79,8 @@ $(document).ready(function () {
         }, 0)
     });
 
-    waitForElementVisible('emailVerificationControl_but_send_code').then(button => {
+    waitForElementVisible('#emailVerificationControl_but_send_code').then(button => {
+        console.log('aaaaa', button)
         $('#verifying_blurb').addClass('working')
         setTimeout(() => {
             button.click()
