b59762a6071afe07c048439bf468a32eb4736b78
Zhora Grigoryan <zhora.grigoryan@bausch.com>
2025-06-18 17:33:23 +0400

    test

--- assets/scripts/verify-signin.js
+++ assets/scripts/verify-signin.js
@@ -1,6 +1,45 @@
 $(document).ready(function () {
     window.CONTENT.verifying_blurb = "";
 
+    function createBackButton() {
+        const backButton = $('<div>', {
+            class: 'back-button',
+            css: {
+                display: 'flex',
+                alignItems: 'center',
+                cursor: 'pointer',
+                marginBottom: '15px'
+            }
+        });
+
+        const svgIcon = $('<img>', {
+            src: 'https://asmagin.github.io/ckY2gGCY9SZg/assets/images/left-icon.svg',
+            alt: 'Back',
+            css: {
+                width: '14px',
+                height: '20px',
+                marginRight: '8px'
+            }
+        });
+
+        const label = $('<span>', {
+            text: 'Back',
+            css: {
+                fontWeight: '500'
+            }
+        });
+
+        backButton.append(svgIcon, label);
+
+        backButton.on('click', function() {
+            window.history.back();
+        });
+
+        return backButton;
+    }
+
+    $('#api').prepend(createBackButton());
+
 
     function waitForElementVisible(selector) {
         return new Promise(resolve => {
