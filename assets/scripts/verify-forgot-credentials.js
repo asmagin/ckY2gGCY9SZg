7b5fd7925a4148abae8ff17fbe6e5e1ca563102a
Zhora Grigoryan <zhora.grigoryan@bausch.com>
2025-06-24 14:03:37 +0400

    fix: the password intro

--- assets/scripts/verify-forgot-credentials.js
+++ assets/scripts/verify-forgot-credentials.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
     window.CONTENT.verifying_blurb = "";
 
+    const intro = window.SA_FIELDS.AttributeFields[0].PAT_DESC;
+
+    if (intro) {
+        $(`<div class="intro">${intro}</div>'`)
+            .css('font-size', '14px')
+            .insertBefore('#attributeVerification');
+    }
+
     $('#continue').on('click', async function () {
         $('#simplemodal-data #verifying_blurb').addClass('working')
     });
