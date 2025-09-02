2cc5d4345dab5d1ebc54817b0449e13cb4b4e70e
Zhora Grigoryan <zhora.grigoryan@bausch.com>
2025-09-02 13:47:54 +0400

    feat: implementing the MFA

---
+++ assets/scripts/verify-phone-code.js
@@ -0,0 +1,92 @@
+$(document).ready(function () {
+    window.CONTENT.verifying_blurb = "";
+
+    const isForgotPasswordPage = $('[data-name="SelfAsserted"]');
+
+    if (!isForgotPasswordPage.length) {
+        return;
+    }
+
+    function waitForElementVisible(selector) {
+        return new Promise(resolve => {
+            if ($(selector).is(':visible')) {
+                resolve();
+                return;
+            }
+
+            const observer = new MutationObserver((mutations, obs) => {
+                if ($(selector).is(':visible')) {
+                    obs.disconnect();
+                    resolve();
+                }
+            });
+
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true,
+                attributes: true,
+                attributeFilter: ['style', 'class']
+            });
+        });
+    }
+
+    function waitForButtonEnabled(buttonId) {
+        return new Promise(resolve => {
+            const button = document.getElementById(buttonId);
+
+            if (button && button.getAttribute('aria-disabled') === 'false') {
+                resolve(button);
+                return;
+            }
+
+            const observer = new MutationObserver((mutations, obs) => {
+                const button = document.getElementById(buttonId);
+                if (button && button.getAttribute('aria-disabled') === 'false') {
+                    obs.disconnect();
+                    resolve(button);
+                }
+            });
+
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true,
+                attributes: true,
+                attributeFilter: ['aria-disabled']
+            });
+        });
+    }
+
+    $('#phoneVerificationControl_but_send_code').on('click', async function () {
+        await waitForElementVisible('.verificationCode_li');
+
+        const introMessage = window?.SA_FIELDS.AttributeFields[0]?.DISPLAY_CONTROL_CONTENT?.intro_msg
+        if(introMessage) {
+            $('#api h1').text(introMessage)
+        }
+
+        $('.phone_li').addClass('none');
+        $('.intro').addClass('none');
+    });
+
+    $('#phoneVerificationControl_but_verify_code').on('click', async function () {
+        await waitForElementVisible('#phoneVerificationControl_but_change_claims');
+
+        $('.phoneVerificationCode_li').addClass('none');
+        const rePassword = $('.reenterPassword_li');
+        const newPassword = $('.newPassword_li');
+        $('#phoneVerificationControl').addClass('none');
+
+        if (rePassword.length && newPassword.length) {
+            rePassword.show();
+            newPassword.show();
+            $('#attributeVerification > .buttons').addClass('flex');
+        }
+    });
+
+    waitForButtonEnabled('continue').then(button => {
+        $('#verifying_blurb').addClass('working')
+        setTimeout(() => {
+            button.click()
+        }, 0)
+    });
+});
